539236833d8869d43a261a81bd6ee80c2d787649
Anthony <asduenas@student.apc.edu.ph>
2026-02-06 21:36:53 +0800

    HOPE LAST UPDATE

--- react-portfolio/src/supabase.js
+++ react-portfolio/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://vranlmqgsbpplufeqhel.supabase.co'
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_w81Fcvpy-IkfMA3e15s7iA_pINASzDB'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
